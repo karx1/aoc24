e49282ca3968aeb04129d26916f74dd88907360a
Yash Karandikar <nerdstep710@gmail.com>
2024-11-30 23:46:38 -0600

    day 1 part 2

--- One.java
+++ One.java
@@ -1,17 +1,16 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Scanner;
-import java.util.TreeSet;
 
 public class One {
     public static void main(String[] args) throws IOException {
         File file = new File("one.txt");
         Scanner scanner = new Scanner(file);
 
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> leftNums = new ArrayList<>();
+        HashMap<Integer, Integer> freqs = new HashMap<>();
 
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
@@ -19,21 +18,24 @@ public class One {
 
             Scanner lineScanner = new Scanner(line);
 
-            list1.add(lineScanner.nextInt());
-            list2.add(lineScanner.nextInt());
+            leftNums.add(lineScanner.nextInt());
+            int num = lineScanner.nextInt();
+
+            Integer current = freqs.get(num);
+            current = current == null ? 1 : current + 1;
+            freqs.put(num, current);
 
             lineScanner.close();
         }
 
-        Collections.sort(list1);
-        Collections.sort(list2);
-
-        int sum = 0;
-        for (int i = 0; i < list1.size(); i++) {
-            sum += Math.abs(list1.get(i) - list2.get(i));
+        int score = 0;
+        for (int i : leftNums) {
+            if (freqs.containsKey(i)) {
+                score += i * freqs.get(i);
+            }
         }
 
         scanner.close();
-        System.out.println(sum);
+        System.out.println(score);
     }
 }
